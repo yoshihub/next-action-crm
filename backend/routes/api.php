5168008d9fdcc78831d26e202cee69135bc574dd
yoshihub <saiga123.nm@gmail.com>
2025-09-20 10:39:18 +0900

    初期設定

---
+++ backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
+
+Route::get('/test', function () {
+    return response()->json(
+        ['message' => 'API動作確認OK'],
+        Response::HTTP_OK,
+        [],
+        JSON_UNESCAPED_UNICODE
+    );
+});
